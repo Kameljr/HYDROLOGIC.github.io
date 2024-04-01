14ccf26a25075c47f711cf1ca32e3d8d1424025e
Kameljr <114341192+Kameljr@users.noreply.github.com>
2024-04-02 02:48:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,53 +64,7 @@ function resultpage() {
     window.location.href = url;
 }
 
- 
-// const slider = document.querySelector('.slider');
-// const button = document.querySelector('button');
-// const leftArrow = document.querySelector('.left');
-// const rightArrow = document.querySelector('.right');
-// const indicatorParents = document.querySelector('.controls ul');
-// var sectionindex = 0;
-// function setIndex() {
-//     document.querySelector('.controls .selected').classList.remove('selected');
-//     slider.style.transform = 'translate(' + (sectionindex) * -20 + '%)';
-//   }
-//   document.querySelectorAll('.controls li').forEach(function (indicator, ind) {
-//     indicator.addEventListener('click', function () {
-//       sectionindex = ind;
-//       setIndex();
-//       indicator.classList.add('selected');
-//     });
-//   });
-//   button.addEventListener('click', resultpage);
-//   function resultpage() {
-//     let sum = 0; // Initialize sum inside the function
-//     let final = 0; // Initialize final inside the function
-//     let ele1 = 0; // Initialize ele1 inside the function
-  
-//     for (let k = 1; k < 2; k++) {
-//       for (let l = 1; l < 5; l++) {
-//         const checkbox = document.getElementById(String(k) + 'o' + String(l));
-//         if (checkbox && checkbox.checked) {
-//           ele1 = Number(checkbox.value);
-//         }
-//       }
-//     }
-//     for (let i = 2; i < 16; i++) {
-//         for (let j = 1; j < 5; j++) {
-//           const option = String(i) + 'o' + String(j);
-//           const checkbox = document.getElementById(option);
-//           if (checkbox && checkbox.checked) {
-//             let ele = Number(checkbox.value);
-//             sum += ele;
-//           }
-//         }
-//       }
-    
-//       final = ele1 * sum;
-//       const result = document.querySelector('#result_para1');
-//       result.innerHTML = `Hi ${final}`;
-//     }   
+
 
 button.addEventListener('click',resultpage)
 function resultpage(){
@@ -133,7 +87,7 @@ function resultpage(){
 
     // Multiply the value of the first question with the total of the other questions
     const result = firstQuestionValue * total;
-    // window.location.href = 'result.html?total=' + result;
+     window.location.href = 'result.html?total=' + result;
     document.getElementById("totalValue").textContent = result;
     if (result>35000){
         document.getElementById("lastpara").innerHTML="The average water footprint of a person is around 35000 litres per month which is less than your water footprint.You need strict guidance on how to reduce water footprint level."
